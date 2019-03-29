ff3a22ce8b7fc8a9bdb988d1aead5c84c3b85751
raducu999 <radustefan1302@gmail.com>
2019-03-29 14:40:24 +0200

    WorldV3

--- Prometheus Society/Assets/Resources/Scripts/MainGame.cs
+++ Prometheus Society/Assets/Resources/Scripts/MainGame.cs
@@ -46,6 +46,10 @@ public class MainGame : MonoBehaviour
     public Text UI_Time;
     public Regions World;
 
+    public RawImage HappynesImg;
+    public RawImage NatureImg;
+    public RawImage IndustryImg;
+
     Color lerpedColor = Color.white;
 
 
@@ -73,6 +77,41 @@ public class MainGame : MonoBehaviour
         World.Australia.Happynes = 0.75f;
         World.Australia.Polution = 0.7f;
         World.Australia.Industry = 0.5f;
+
+        World.SAfrica.GraphicComponent = GameObject.Find("S.Africa");
+        World.SAfrica.Happynes = 0.25f;
+        World.SAfrica.Polution = 0.45f;
+        World.SAfrica.Industry = 0.35f;
+
+        World.NAsia.GraphicComponent = GameObject.Find("N.Asia");
+        World.NAsia.Happynes = 0.55f;
+        World.NAsia.Polution = 0.5f;
+        World.NAsia.Industry = 0.6f;
+
+        World.SAsia.GraphicComponent = GameObject.Find("S.Asia");
+        World.SAsia.Happynes = 0.45f;
+        World.SAsia.Polution = 0.3f;
+        World.SAsia.Industry = 0.85f;
+
+        World.Antarctica.GraphicComponent = GameObject.Find("Antarctica");
+        World.Antarctica.Happynes = 0.65f;
+        World.Antarctica.Polution = 0.95f;
+        World.Antarctica.Industry = 0.1f;
+
+        World.IcelandGreenland.GraphicComponent = GameObject.Find("Greenland-Iceland");
+        World.IcelandGreenland.Happynes = 0.9f;
+        World.IcelandGreenland.Polution = 0.85f;
+        World.IcelandGreenland.Industry = 0.25f;
+
+        World.NAmerica.GraphicComponent = GameObject.Find("N.America");
+        World.NAmerica.Happynes = 0.7f;
+        World.NAmerica.Polution = 0.65f;
+        World.NAmerica.Industry = 0.8f;
+
+        World.SAmerica.GraphicComponent = GameObject.Find("S.America");
+        World.SAmerica.Happynes = 0.55f;
+        World.SAmerica.Polution = 0.55f;
+        World.SAmerica.Industry = 0.5f;
     }
 
 
@@ -135,35 +174,110 @@ public class MainGame : MonoBehaviour
     }
 
     public void DisplayHappynes () {
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.Europe.Happynes);
+        HappynesImg.color = Color.yellow;
+        NatureImg.color = Color.white;
+        IndustryImg.color = Color.white;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Europe.Happynes);
         World.Europe.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
 
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.AfricaAarabia.Happynes);
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.AfricaAarabia.Happynes);
         World.AfricaAarabia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
 
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.Australia.Happynes);
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Australia.Happynes);
         World.Australia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAfrica.Happynes);
+        World.SAfrica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.NAsia.Happynes);
+        World.NAsia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAsia.Happynes);
+        World.SAsia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Antarctica.Happynes);
+        World.Antarctica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.IcelandGreenland.Happynes);
+        World.IcelandGreenland.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.NAmerica.Happynes);
+        World.NAmerica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAmerica.Happynes);
+        World.SAmerica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
     }
 
     public void DisplayPolution () {
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.Europe.Polution);
+        HappynesImg.color = Color.white;
+        NatureImg.color = Color.yellow;
+        IndustryImg.color = Color.white;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Europe.Polution);
         World.Europe.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
 
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.AfricaAarabia.Polution);
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.AfricaAarabia.Polution);
         World.AfricaAarabia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
 
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.Australia.Polution);
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Australia.Polution);
         World.Australia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAfrica.Polution);
+        World.SAfrica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.NAsia.Polution);
+        World.NAsia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAsia.Polution);
+        World.SAsia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Antarctica.Polution);
+        World.Antarctica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.IcelandGreenland.Polution);
+        World.IcelandGreenland.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.NAmerica.Polution);
+        World.NAmerica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAmerica.Polution);
+        World.SAmerica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
     }
 
     public void DisplayIndustry () {
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.Europe.Industry);
+        HappynesImg.color = Color.white;
+        NatureImg.color = Color.white;
+        IndustryImg.color = Color.yellow;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Europe.Industry);
         World.Europe.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
 
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.AfricaAarabia.Industry);
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.AfricaAarabia.Industry);
         World.AfricaAarabia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
 
-        lerpedColor = Color.Lerp(Color.red, Color.green, World.Australia.Industry);
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Australia.Industry);
         World.Australia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAfrica.Industry);
+        World.SAfrica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+        
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.NAsia.Industry);
+        World.NAsia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAsia.Industry);
+        World.SAsia.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.Antarctica.Industry);
+        World.Antarctica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.IcelandGreenland.Industry);
+        World.IcelandGreenland.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+        
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.NAmerica.Industry);
+        World.NAmerica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
+
+        lerpedColor = Color.Lerp(Color.red, Color.blue, World.SAmerica.Industry);
+        World.SAmerica.GraphicComponent.GetComponent<SpriteRenderer>().color = lerpedColor;
     }
 }
